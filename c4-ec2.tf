98160e4ba60e3a12a02e58f31940ed6ba7e9096f
Hrishikesh Panigrahi <30368891+hrishipanigrahi@users.noreply.github.com>
2023-02-17 01:08:30 +0530

    Create c4-ec2.tf

---
+++ c4-ec2.tf
@@ -0,0 +1,12 @@
+# Resource ec2
+resource "aws_instance" "web-ec2" {
+  ami           = data.aws_ami.latest_ami.id
+  instance_type = var.instance_type
+  key_name      = "new-key"
+  user_data     = file("apache.sh")
+  count         = var.instance_count
+  tags = {
+    Name = "web-ec2-${count.index}"
+  }
+  vpc_security_group_ids = [aws_security_group.my-SG-1.id]
+}
