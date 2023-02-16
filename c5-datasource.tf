e5055fc527c16ae0578d5d7f4885bf4bcda42b1b
Hrishikesh Panigrahi <30368891+hrishipanigrahi@users.noreply.github.com>
2023-02-17 01:08:53 +0530

    Create c5-datasource.tf

---
+++ c5-datasource.tf
@@ -0,0 +1,25 @@
+# datasource
+data "aws_ami" "latest_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+}
